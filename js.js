ed30b56c23a281d2436071349af9abf79679c739
Marta <martafontb@gmail.com>
2021-04-11 20:12:48 +0200

    media query added

--- js.js
+++ js.js
@@ -8,22 +8,22 @@ function getRandom(min, max) {
   }
 
 
-
-
-
 // With all the shapes inside the <div> read into an array, I can loop through each one:
 preloadedShapes.forEach(function(shape) {   
     shape.style.position = "absolute";
+    shape.style.transform = "scale(0.55) rotate(" + ( Math.random() * 20 - 10) + "deg)";
     shape.style.width = getRandom(33,45)+"%";
     shape.style.left = getRandom(5,65)+"%";
     shape.style.right = getRandom(5,65)+"%";
     shape.style.top = getRandom(6,60)+"vh";
     shape.style.bottom = getRandom(6,60)+"vh";
-    shape.style.transform = "scale(0.6) rotate(" + ( Math.random() * 20 - 10) + "deg)";
     shape.classList.add("hide");
 })
 
 
-
-
-
+const mediaQuery = window.matchMedia('(max-width: 768px)')
+if (mediaQuery.matches) {
+    preloadedShapes.forEach(function(shape) {  
+        shape.style.transform = "scale(1) rotate(" + ( Math.random() * 20 - 10) + "deg)";
+    })
+}
