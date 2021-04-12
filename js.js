909fea97006c6a9ea099a1fa993d4a6294d775d7
Marta <martafontb@gmail.com>
2021-04-12 09:57:23 +0200

    added NodeClone

--- js.js
+++ js.js
@@ -1,19 +1,30 @@
 const preload = document.querySelector(".preload")
 const preloadedShapes = preload.querySelectorAll("img")
 
-//shapes at random on the page
+
+//random num generator
 function getRandom(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
   }
+ 
+//clone shapes
+function clone() {
+    const shape1 = preload.querySelector('.shape1');
+    for(let i=0; i < getRandom(20, 50); i++) 
+    preload.appendChild(shape1.cloneNode(true));
+ }
+clone();
 
-  // With all the shapes inside the <div> read into an array, I can loop through each one:
+
+// With all the shapes inside the <div> read into an array, I can loop through each one:
 preloadedShapes.forEach(function(shape) {   
     shape.style.position = "absolute";
     shape.style.transform = "scale(0.55) rotate(" + ( Math.random() * 20 - 10) + "deg)";
-    shape.style.width = getRandom(33, 70)+"%";
+    shape.style.width = getRandom(33, 75)+"%";
     shape.style.left = getRandom(-20, 100)+"%";
-    shape.style.top = getRandom(-20, 100)+"vh";
+    shape.style.top = getRandom(-20, 100)+"vh"; 
 })
+
 
 
 const mediaQuery = window.matchMedia('(max-width: 768px)')
