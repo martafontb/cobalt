9077d6b3b121f1221cc0baff8f3889284259caf3
Marta <martafontb@gmail.com>
2021-04-12 08:50:43 +0200

    changed values to fill up more space

--- js.js
+++ js.js
@@ -10,9 +10,9 @@ function getRandom(min, max) {
 preloadedShapes.forEach(function(shape) {   
     shape.style.position = "absolute";
     shape.style.transform = "scale(0.55) rotate(" + ( Math.random() * 20 - 10) + "deg)";
-    shape.style.width = getRandom(33,45)+"%";
-    shape.style.left = getRandom(-5,65)+"%";
-    shape.style.top = getRandom(-6,60)+"vh";
+    shape.style.width = getRandom(33, 70)+"%";
+    shape.style.left = getRandom(-20, 100)+"%";
+    shape.style.top = getRandom(-20, 100)+"vh";
 })
 
 
