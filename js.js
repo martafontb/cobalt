bfd838d977e1f2a22eef41313adb1296deef92e9
Marta <martafontb@gmail.com>
2021-04-12 10:11:04 +0200

    changed cloneNode

--- js.js
+++ js.js
@@ -6,18 +6,11 @@ const preloadedShapes = preload.querySelectorAll("img")
 function getRandom(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
   }
- 
-//clone shapes
-function clone() {
-    const shape1 = preload.querySelector('.shape1');
-    for(let i=0; i < getRandom(20, 50); i++) 
-    preload.appendChild(shape1.cloneNode(true));
- }
-clone();
 
 
 // With all the shapes inside the <div> read into an array, I can loop through each one:
-preloadedShapes.forEach(function(shape) {   
+preloadedShapes.forEach((shape) => {  
+    preload.appendChild(shape.cloneNode(true));
     shape.style.position = "absolute";
     shape.style.transform = "scale(0.55) rotate(" + ( Math.random() * 20 - 10) + "deg)";
     shape.style.width = getRandom(33, 75)+"%";
